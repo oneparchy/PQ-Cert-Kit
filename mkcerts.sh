231b8a710720ef0e9378912ce8c7cf819a89d1e0
oneparchy <98184747+oneparchy@users.noreply.github.com>
2025-03-14 22:24:13 -0400

    Update mkcerts.sh
    Updated to work with new format via OQS provider

--- mkcerts.sh
+++ mkcerts.sh
@@ -2,11 +2,8 @@
 
 # Script to:
 # 1. Create various Root CA, Intermediate CA, OCSP signing, Web Server, & Web Client certificates
-#	included signature algorithms: RSA, ECDSA, EdDSA, Falcon512 & 1024, Dilithium 2, 3 & 5, SPHINCS+-SHAKE256, SPHINCS+-SHA-256, SPHINCS+-Haraka
 
-# Requires OQS OpenSSL installed
-
-BASE_DIR=$(pwd)
+# Requires OQS Provider installed and activated
 
 ###############################################################################
 ################################## IMPORTANT ##################################
@@ -15,22 +12,15 @@ BASE_DIR=$(pwd)
 
 # You may also need to edit the openssl.cnf file included in this repo to set
 # the OCSP URL. Look for the lines that say:
-
-# ---------------EDIT BELOW WITH YOUR DESIRED OCSP SERVER URL
-# authorityInfoAccess    = OCSP;URI:http://ocsp:8080	
-
+# 	---------------EDIT BELOW WITH YOUR DESIRED OCSP SERVER URL
+# 	authorityInfoAccess    = OCSP;URI:http://ocsp:8080	
 # under the relevant x509 extension headers section. 
 
-#ABSOLUTE path to the OQS OpenSSL executable:
-qopenssl=$BASE_DIR/openssl/apps/openssl
-
-#ABSOLUTE path to openssl config file (openssl.cnf):
-conffile=$BASE_DIR/openssl.cnf
-#You will have to change a lot in this script f you don't use the config file included with this repo
-
 #Algorithms to generate certs with:
-ALGS=("rsa" "ecdsa" "eddsa" "falcon512" "falcon1024" "dilithium2" "dilithium3" "dilithium5" "sphincssha256128ssimple" "sphincsshake256128fsimple" "rsa3072_falcon512" "p256_falcon512" "p521_falcon1024" "rsa3072_dilithium2" "p256_dilithium2" "p384_dilithium3" "p521_dilithium5" "rsa3072_sphincssha256128ssimple" "p256_sphincssha256128ssimple" "rsa3072_sphincsshake256128fsimple" "p256_sphincsshake256128fsimple")
-# rsa, ecdsa, & eddsa are specially coded - for PQ algorithms, make sure the name matches exactly with what is listed by liboqs, and it should work if OpenSSL has been compiled with support for those algorithms
+ALGS=("rsa" "ecdsa" "eddsa" "falcon512" "falcon1024" "mldsa44" "mldsa65" "mldsa87" "sphincssha2128fsimple" "sphincssha2128ssimple" "sphincsshake128fsimple" "rsa3072_falcon512" "p256_falcon512" "p521_falcon1024" "rsa3072_mldsa44" "p256_mldsa44" "p384_mldsa65" "p521_mldsa87" "rsa3072_sphincssha2128fsimple" "p256_sphincssha2128fsimple" "rsa3072_sphincssha2128ssimple" "p256_sphincssha2128ssimple" "rsa3072_sphincsshake128fsimple" "p256_sphincsshake128fsimple")
+# rsa, ecdsa, & eddsa are specially coded below - for PQ algorithms, make sure the name matches exactly with what is listed by liboqs, and it should work if LibOQS has been built with those algorithms included
+
+BASE_DIR=$(pwd)
 
 #--------------------------- CERTIFICATE SUBJECT DN ---------------------------
 declare -A subject
@@ -148,18 +138,18 @@ for ALG in ${ALGS[@]}; do
 	#ROOT CA
 	_info "Generating Root CA cert..."
 	if [ $ALG == "rsa" ]; then
-		$qopenssl req -x509 -config $conffile -newkey rsa:4096 -sha256 -subj "${subject[root]}" -out root/cacert.pem -passout pass:"${pass[root]}" -keyout root/cakey.pem -extensions v3_rca -days 730 -batch;
+		openssl req -x509 -newkey rsa:4096 -sha256 -subj "${subject[root]}" -out root/cacert.pem -passout pass:"${pass[root]}" -keyout root/cakey.pem -extensions v3_rca -days 730 -batch;
 	elif [ $ALG == "ecdsa" ]; then
-		$qopenssl ecparam -name prime256v1 -genkey -out root/cakey.pem;				#Generate EC private key
-		$qopenssl ec -in root/cakey.pem -out root/cakey.pem -aes256 -passout pass:"${pass[root]}";		#Encrypt it
-		$qopenssl req -x509 -config $conffile -key root/cakey.pem -passin pass:"${pass[root]}" -sha256 -out root/cacert.pem -subj "${subject[root]}" -extensions v3_rca -days 730 -batch;
+		openssl ecparam -name prime256v1 -genkey -out root/cakey.pem;				#Generate EC private key
+		openssl ec -in root/cakey.pem -out root/cakey.pem -aes256 -passout pass:"${pass[root]}";		#Encrypt it
+		openssl req -x509 -key root/cakey.pem -passin pass:"${pass[root]}" -sha256 -out root/cacert.pem -subj "${subject[root]}" -extensions v3_rca -days 730 -batch;
 	elif [ $ALG == "eddsa" ]; then
-		$qopenssl genpkey -algorithm ED25519 -out root/cakey.pem -pass pass:"${pass[root]}";		#Generate ED25519 encrypted private key
-		$qopenssl req -x509 -config $conffile -key root/cakey.pem -passin pass:"${pass[root]}" -sha256 -out root/cacert.pem -subj "${subject[root]}" -extensions v3_rca -days 730 -batch;
+		openssl genpkey -algorithm ED25519 -out root/cakey.pem -pass pass:"${pass[root]}";		#Generate ED25519 encrypted private key
+		openssl req -x509 -key root/cakey.pem -passin pass:"${pass[root]}" -sha256 -out root/cacert.pem -subj "${subject[root]}" -extensions v3_rca -days 730 -batch;
 	else
-		$qopenssl req -x509 -config $conffile -newkey $ALG -sha256 -subj "${subject[root]}" -out root/cacert.pem -passout pass:"${pass[root]}" -keyout root/cakey.pem -extensions v3_rca -days 730 -batch;
+		openssl req -x509 -newkey $ALG -sha256 -subj "${subject[root]}" -out root/cacert.pem -passout pass:"${pass[root]}" -keyout root/cakey.pem -extensions v3_rca -days 730 -batch;
 	fi
-	#$qopenssl x509 -purpose -in root/cacert.pem ;
+	#openssl x509 -purpose -in root/cacert.pem ;
 	if [ $? -eq 0 ]; then
 		_success "Root CA cert/key generated"
 	else
@@ -170,21 +160,21 @@ for ALG in ${ALGS[@]}; do
 	#INTERMEDIATE CA
 	_info "Generating Intermediate CA cert..."
 	if [ $ALG == "rsa" ]; then
-		$qopenssl req -new -config $conffile -newkey rsa:4096 -sha256 -subj "${subject[inter]}" -out inter/cacert.csr -passout pass:"${pass[inter]}" -keyout inter/cakey.pem -batch;
+		openssl req -new -newkey rsa:4096 -sha256 -subj "${subject[inter]}" -out inter/cacert.csr -passout pass:"${pass[inter]}" -keyout inter/cakey.pem -batch;
 	elif [ $ALG == "ecdsa" ]; then
-		$qopenssl ecparam -name prime256v1 -genkey -out inter/cakey.pem;				#Generate EC private key
-		$qopenssl ec -in inter/cakey.pem -out inter/cakey.pem -aes256 -passout pass:"${pass[inter]}";	#Encrypt it
-		$qopenssl req -new -config $conffile -key inter/cakey.pem -passin pass:"${pass[inter]}" -sha256 -out inter/cacert.csr -subj "${subject[inter]}" -batch;
+		openssl ecparam -name prime256v1 -genkey -out inter/cakey.pem;				#Generate EC private key
+		openssl ec -in inter/cakey.pem -out inter/cakey.pem -aes256 -passout pass:"${pass[inter]}";	#Encrypt it
+		openssl req -new -key inter/cakey.pem -passin pass:"${pass[inter]}" -sha256 -out inter/cacert.csr -subj "${subject[inter]}" -batch;
 	elif [ $ALG == "eddsa" ]; then
-		$qopenssl genpkey -algorithm ED25519 -out inter/cakey.pem -pass pass:"${pass[inter]}";		#Generate ED25519 encrypted private key
-		$qopenssl req -new -config $conffile -key inter/cakey.pem -passin pass:"${pass[inter]}" -sha256 -out inter/cacert.csr -subj "${subject[inter]}" -batch;
+		openssl genpkey -algorithm ED25519 -out inter/cakey.pem -pass pass:"${pass[inter]}";		#Generate ED25519 encrypted private key
+		openssl req -new -key inter/cakey.pem -passin pass:"${pass[inter]}" -sha256 -out inter/cacert.csr -subj "${subject[inter]}" -batch;
 	else
-		$qopenssl req -new -config $conffile -newkey $ALG -sha256 -subj "${subject[inter]}" -out inter/cacert.csr -passout pass:"${pass[inter]}" -keyout inter/cakey.pem -batch;
+		openssl req -new -newkey $ALG -sha256 -subj "${subject[inter]}" -out inter/cacert.csr -passout pass:"${pass[inter]}" -keyout inter/cakey.pem -batch;
 	fi
-	#$qopenssl req -text -noout -verify -in inter/cacert.csr -config $conffile;
-	$qopenssl ca -config $conffile -cert root/cacert.pem -keyfile root/cakey.pem -passin pass:"${pass[root]}" -extensions v3_ica -out inter/cacert.pem -batch -days 365 -infiles inter/cacert.csr;
-	#$qopenssl x509 -purpose -in inter/cacert.pem ;
-	$qopenssl verify -verbose -CAfile root/cacert.pem inter/cacert.pem;
+	#openssl req -text -noout -verify -in inter/cacert.csr -config $conffile;
+	openssl ca -name RootCA -cert root/cacert.pem -keyfile root/cakey.pem -passin pass:"${pass[root]}" -extensions v3_ica -out inter/cacert.pem -batch -days 365 -infiles inter/cacert.csr;
+	#openssl x509 -purpose -in inter/cacert.pem ;
+	openssl verify -verbose -CAfile root/cacert.pem inter/cacert.pem;
 	if [ $? -eq 0 ]; then
 		_success "Intermediate CA cert/key generated"
 	else
@@ -195,21 +185,21 @@ for ALG in ${ALGS[@]}; do
 	#OCSP
 	_info "Generating OCSP signing cert..."
 	if [ $ALG == "rsa" ]; then
-		$qopenssl req -new -config $conffile -newkey rsa:4096 -sha256 -subj "${subject[ocsp]}" -out ocspcert.csr -passout pass:"${pass[ocsp]}" -keyout ocspkey.pem -batch;
+		openssl req -new -newkey rsa:4096 -sha256 -subj "${subject[ocsp]}" -out ocspcert.csr -passout pass:"${pass[ocsp]}" -keyout ocspkey.pem -batch;
 	elif [ $ALG == "ecdsa" ]; then
-		$qopenssl ecparam -name prime256v1 -genkey -out ocspkey.pem;			#Generate EC private key
-		$qopenssl ec -in ocspkey.pem -out ocspkey.pem -aes256 -passout pass:"${pass[ocsp]}";	#Encrypt it
-		$qopenssl req -new -config $conffile -key ocspkey.pem -passin pass:"${pass[ocsp]}" -sha256 -out ocspcert.csr -subj "${subject[ocsp]}" -batch;
+		openssl ecparam -name prime256v1 -genkey -out ocspkey.pem;			#Generate EC private key
+		openssl ec -in ocspkey.pem -out ocspkey.pem -aes256 -passout pass:"${pass[ocsp]}";	#Encrypt it
+		openssl req -new -key ocspkey.pem -passin pass:"${pass[ocsp]}" -sha256 -out ocspcert.csr -subj "${subject[ocsp]}" -batch;
 	elif [ $ALG == "eddsa" ]; then
-		$qopenssl genpkey -algorithm ED25519 -out ocspkey.pem -pass pass:"${pass[ocsp]}";		#Generate ED25519 encrypted private key
-		$qopenssl req -new -config $conffile -key ocspkey.pem -passin pass:"${pass[ocsp]}" -sha256 -out ocspcert.csr -subj "${subject[ocsp]}" -batch;
+		openssl genpkey -algorithm ED25519 -out ocspkey.pem -pass pass:"${pass[ocsp]}";		#Generate ED25519 encrypted private key
+		openssl req -new -key ocspkey.pem -passin pass:"${pass[ocsp]}" -sha256 -out ocspcert.csr -subj "${subject[ocsp]}" -batch;
 	else
-		$qopenssl req -new -config $conffile -newkey $ALG -sha256 -subj "${subject[ocsp]}" -out ocspcert.csr -passout pass:"${pass[ocsp]}" -keyout ocspkey.pem -batch;
+		openssl req -new -newkey $ALG -sha256 -subj "${subject[ocsp]}" -out ocspcert.csr -passout pass:"${pass[ocsp]}" -keyout ocspkey.pem -batch;
 	fi
-	#$qopenssl req -text -noout -verify -in ocspcert.csr -config $conffile;
-	$qopenssl ca -config $conffile -name InterCA -cert inter/cacert.pem -keyfile inter/cakey.pem -passin pass:"${pass[inter]}" -extensions v3_ocsp -out ocspcert.pem -batch -days 365 -infiles ocspcert.csr;
-	#$qopenssl x509 -purpose -in ocspcert.pem ;
-	$qopenssl verify -verbose -CAfile root/cacert.pem -untrusted inter/cacert.pem ocspcert.pem;
+	#openssl req -text -noout -verify -in ocspcert.csr -config $conffile;
+	openssl ca -name InterCA -cert inter/cacert.pem -keyfile inter/cakey.pem -passin pass:"${pass[inter]}" -extensions v3_ocsp -out ocspcert.pem -batch -days 365 -infiles ocspcert.csr;
+	#openssl x509 -purpose -in ocspcert.pem ;
+	openssl verify -verbose -CAfile root/cacert.pem -untrusted inter/cacert.pem ocspcert.pem;
 	if [ $? -eq 0 ]; then
 		_success "OCSP cert/key generated"
 	else
@@ -220,21 +210,21 @@ for ALG in ${ALGS[@]}; do
 	#HTTPS SVR
 	_info "Generating HTTPS Server cert..."
 	if [ $ALG == "rsa" ]; then
-		$qopenssl req -new -config $conffile -newkey rsa:4096 -sha256 -subj "${subject[svr]}" -out svrcert.csr -passout pass:"${pass[svr]}" -keyout svrkey.pem -batch;
+		openssl req -new -newkey rsa:4096 -sha256 -subj "${subject[svr]}" -out svrcert.csr -passout pass:"${pass[svr]}" -keyout svrkey.pem -batch;
 	elif [ $ALG == "ecdsa" ]; then
-		$qopenssl ecparam -name prime256v1 -genkey -out svrkey.pem;				#Generate EC private key
-		$qopenssl ec -in svrkey.pem -out svrkey.pem -aes256 -passout pass:"${pass[svr]}";		#Encrypt it
-		$qopenssl req -new -config $conffile -key svrkey.pem -passin pass:"${pass[svr]}" -sha256 -out svrcert.csr -subj "${subject[svr]}" -batch;
+		openssl ecparam -name prime256v1 -genkey -out svrkey.pem;				#Generate EC private key
+		openssl ec -in svrkey.pem -out svrkey.pem -aes256 -passout pass:"${pass[svr]}";		#Encrypt it
+		openssl req -new -key svrkey.pem -passin pass:"${pass[svr]}" -sha256 -out svrcert.csr -subj "${subject[svr]}" -batch;
 	elif [ $ALG == "eddsa" ]; then
-		$qopenssl genpkey -algorithm ED25519 -out svrkey.pem -pass pass:"${pass[svr]}";		#Generate ED25519 encrypted private key
-		$qopenssl req -new -config $conffile -key svrkey.pem -passin pass:"${pass[svr]}" -sha256 -out svrcert.csr -subj "${subject[svr]}" -batch;
+		openssl genpkey -algorithm ED25519 -out svrkey.pem -pass pass:"${pass[svr]}";		#Generate ED25519 encrypted private key
+		openssl req -new -key svrkey.pem -passin pass:"${pass[svr]}" -sha256 -out svrcert.csr -subj "${subject[svr]}" -batch;
 	else
-		$qopenssl req -new -config $conffile -newkey $ALG -sha256 -subj "${subject[svr]}" -out svrcert.csr -passout pass:"${pass[svr]}" -keyout svrkey.pem -batch;
+		openssl req -new -newkey $ALG -sha256 -subj "${subject[svr]}" -out svrcert.csr -passout pass:"${pass[svr]}" -keyout svrkey.pem -batch;
 	fi
-	#$qopenssl req -text -noout -verify -in svrcert.csr -config $conffile;
-	$qopenssl ca -config $conffile -name InterCA -cert inter/cacert.pem -keyfile inter/cakey.pem -passin pass:"${pass[inter]}" -extensions v3_server -out svrcert.pem -batch -days 365 -infiles svrcert.csr;
-	#$qopenssl x509 -purpose -in svrcert.pem ;
-	$qopenssl verify -verbose -CAfile root/cacert.pem -untrusted inter/cacert.pem svrcert.pem;
+	#openssl req -text -noout -verify -in svrcert.csr -config $conffile;
+	openssl ca -name InterCA -cert inter/cacert.pem -keyfile inter/cakey.pem -passin pass:"${pass[inter]}" -extensions v3_server -out svrcert.pem -batch -days 365 -infiles svrcert.csr;
+	#openssl x509 -purpose -in svrcert.pem ;
+	openssl verify -verbose -CAfile root/cacert.pem -untrusted inter/cacert.pem svrcert.pem;
 	if [ $? -eq 0 ]; then
 		_success "Server cert/key generated"
 	else
@@ -245,21 +235,21 @@ for ALG in ${ALGS[@]}; do
 	#HTTPS CLIENT
 	_info "Generating HTTPS Client cert..."
 	if [ $ALG == "rsa" ]; then
-		$qopenssl req -new -config $conffile -newkey rsa:4096 -sha256 -subj "${subject[usr]}" -out usrcert.csr -passout pass:"${pass[usr]}" -keyout usrkey.pem -batch;
+		openssl req -new -newkey rsa:4096 -sha256 -subj "${subject[usr]}" -out usrcert.csr -passout pass:"${pass[usr]}" -keyout usrkey.pem -batch;
 	elif [ $ALG == "ecdsa" ]; then
-		$qopenssl ecparam -name prime256v1 -genkey -out usrkey.pem;				#Generate EC private key
-		$qopenssl ec -in usrkey.pem -out usrkey.pem -aes256 -passout pass:"${pass[usr]}";		#Encrypt it
-		$qopenssl req -new -config $conffile -key usrkey.pem -passin pass:"${pass[usr]}" -sha256 -out usrcert.csr -subj "${subject[usr]}" -batch;
+		openssl ecparam -name prime256v1 -genkey -out usrkey.pem;				#Generate EC private key
+		openssl ec -in usrkey.pem -out usrkey.pem -aes256 -passout pass:"${pass[usr]}";		#Encrypt it
+		openssl req -new -key usrkey.pem -passin pass:"${pass[usr]}" -sha256 -out usrcert.csr -subj "${subject[usr]}" -batch;
 	elif [ $ALG == "eddsa" ]; then
-		$qopenssl genpkey -algorithm ED25519 -out usrkey.pem -pass pass:"${pass[usr]}";		#Generate ED25519 encrypted private key
-		$qopenssl req -new -config $conffile -key usrkey.pem -passin pass:"${pass[usr]}" -sha256 -out usrcert.csr -subj "${subject[usr]}" -batch;
+		openssl genpkey -algorithm ED25519 -out usrkey.pem -pass pass:"${pass[usr]}";		#Generate ED25519 encrypted private key
+		openssl req -new -key usrkey.pem -passin pass:"${pass[usr]}" -sha256 -out usrcert.csr -subj "${subject[usr]}" -batch;
 	else
-		$qopenssl req -new -config $conffile -newkey $ALG -sha256 -subj "${subject[usr]}" -out usrcert.csr -passout pass:"${pass[usr]}" -keyout usrkey.pem -batch;
+		openssl req -new -newkey $ALG -sha256 -subj "${subject[usr]}" -out usrcert.csr -passout pass:"${pass[usr]}" -keyout usrkey.pem -batch;
 	fi
-	#$qopenssl req -text -noout -verify -in usrcert.csr -config $conffile;
-	$qopenssl ca -config $conffile -name InterCA -cert inter/cacert.pem -keyfile inter/cakey.pem -passin pass:"${pass[inter]}" -extensions v3_client -out usrcert.pem -batch -days 365 -infiles usrcert.csr;
-	#$qopenssl x509 -purpose -in usrcert.pem ;
-	$qopenssl verify -verbose -CAfile root/cacert.pem -untrusted inter/cacert.pem usrcert.pem;
+	#openssl req -text -noout -verify -in usrcert.csr -config $conffile;
+	openssl ca -name InterCA -cert inter/cacert.pem -keyfile inter/cakey.pem -passin pass:"${pass[inter]}" -extensions v3_client -out usrcert.pem -batch -days 365 -infiles usrcert.csr;
+	#openssl x509 -purpose -in usrcert.pem ;
+	openssl verify -verbose -CAfile root/cacert.pem -untrusted inter/cacert.pem usrcert.pem;
 	if [ $? -eq 0 ]; then
 		_success "Client cert/key generated"
 	else
@@ -269,7 +259,7 @@ for ALG in ${ALGS[@]}; do
 done
 
 if [ $haserr -eq 0 ]; then
-	_success "All certificates generated successfully"
+	_success "All certificates generated successfully" 
 else
 	_warn "Failed to generate some certificates"
 fi
